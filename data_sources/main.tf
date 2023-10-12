7b921334d8fbd399e10e33ea868041de0f0fa339
chinnapareddy <chinna.aduri@gmail.com>
2023-10-12 09:23:03 -0700

    data source

--- data_sources/main.tf
+++ data_sources/main.tf
@@ -1,6 +1,6 @@
 data "aws_ami" "ami" {
   most_recent = true
   // name_regex  = "Centos.*"
-  ami_id = ami-07ecd1d0c2a8a881d
+  ami_id = "ami-07ecd1d0c2a8a881d"
   owners      = ["683468297985"]
 }

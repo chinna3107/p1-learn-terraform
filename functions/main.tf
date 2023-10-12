dd19ef7a66b0a9ebdfa19c0a127f3da9ad20651d
chinnapareddy <chinna.aduri@gmail.com>
2023-10-12 08:32:32 -0700

    functions

---
+++ functions/main.tf
@@ -0,0 +1,24 @@
+variable "fruits" {
+  default = [ "apple", "banana"]
+}
+
+output "fruit" {
+  //value =  var.fruits[2]
+  value = element(var.fruits,2)
+}
+
+variable "fruit_with_Stock" {
+  default = {
+     apple = 100
+  }
+}
+
+output "fruit_stock"{
+  value = try (var.fruit_with_Stock["banana"],0)
+}
+
+output "fruit_stock_price" {
+
+  value = lookup(var.fruit_with_Stock["apple"].price,2)
+
+}

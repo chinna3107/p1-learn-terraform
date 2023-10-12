1b7113cb172dd18fbd5fdfbbfe06625409f7990a
chinnapareddy <chinna.aduri@gmail.com>
2023-10-11 20:59:32 -0700

    Variable

--- intro/sample.tf
+++ intro/sample.tf
@@ -11,5 +11,37 @@ output "fruit_name" {
   value=var.fruit_name
 }
 
+variable "fruits" {
+  default = [
+    "apple",
+    "banana"
+  ]
+  // default = [ "apple", "banana"]
+}
+
+# map variable, plain
+variable "fruit_stock" {
+
+  default = {
+    apple = 100
+    banana = 200
+   }
+}
+
+# map variable, map of map
+variable "fruit_stock_with_price" {
+  default = {
+    apple = {
+      stock = 100
+      price = 20
+    }
+    banana = {
+      stock = 200
+      price = 5
+    }
+  }
+}
+
+
 
 

49b6ddb50d840557c8324db4b073707d009448ae
chinnapareddy <chinna.aduri@gmail.com>
2023-10-13 05:38:32 -0700

    Count

--- count/main.tf
+++ count/main.tf
@@ -1,20 +1,20 @@
 variable "components" {
   default = ["catalogue", "frontend", "mongodb"]
 }
-resource "aws_instance" "instance" {
+// resource "aws_instance" "instance" {
 
+// count = length(var.components)
+
+//  ami                    = "ami-07ecd1d0c2a8a881d"
+//  instance_type          = "t3.small"
+//  vpc_security_group_ids = [ "sg-0c786c9e56b3a46f3" ]
+
+//  tags = {
+//    Name = element(var.components, count.index)
+//  }
+//}
+
+resource "aws_security_group" "allow_lts" {
   count = length(var.components)
-
-  ami                    = "ami-07ecd1d0c2a8a881d"
-  instance_type          = "t3.small"
-  vpc_security_group_ids = [ "sg-0c786c9e56b3a46f3" ]
-
-  tags = {
-    Name = element(var.components, count.index)
-  }
-}
-
-//resource "aws_security_group" "allow_lts" {
-//  count = length(var.components)
-//   name = element(var.components, count.index)
-// }
+   name = element(var.components, count.index)
+ }

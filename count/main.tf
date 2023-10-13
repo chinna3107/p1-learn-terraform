5abf90d778f7878d5b60fdafff0a7b12fd01db13
chinnapareddy <chinna.aduri@gmail.com>
2023-10-13 04:38:07 -0700

    data source

---
+++ count/main.tf
@@ -0,0 +1,15 @@
+variable "components" {
+  default = ["frontend", "mongodb"]
+}
+resource "aws_instance" "ami" {
+
+  count = length(var.components)
+
+  ami                    = "ami-07ecd1d0c2a8a881d"
+  instance_type          = "t3.small"
+  vpc_security_group_ids = ["sg-0c786c9e56b3a46f3"]
+
+  tags = {
+    Name = element(var.components, count.index)
+  }
+}

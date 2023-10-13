5bbca0d5d21b81c342f0c1c4167a9b4e6aa7c467
chinnapareddy <chinna.aduri@gmail.com>
2023-10-13 05:16:42 -0700

    Count

--- count/main.tf
+++ count/main.tf
@@ -1,13 +1,13 @@
 variable "components" {
   default = ["frontend", "mongodb"]
 }
-resource "aws_instance" "ami" {
+resource "aws_instance" "instance" {
 
   count = length(var.components)
 
   ami                    = "ami-07ecd1d0c2a8a881d"
   instance_type          = "t3.small"
-  vpc_security_group_ids = ["sg-0c786c9e56b3a46f3"]
+  vpc_security_group_ids = [ "sg-0c786c9e56b3a46f3" ]
 
   tags = {
     Name = element(var.components, count.index)

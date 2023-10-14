15ebb33b03808dffd4ad8ccddd70f6887aca62c4
chinnapareddy <chinna.aduri@gmail.com>
2023-10-14 04:03:47 -0700

    Module

--- module/local-module/data.tf
+++ module/local-module/data.tf
@@ -11,9 +11,9 @@ output "ami" {
 
 resource "aws_instance" "instance" {
    ami                     = data.aws_ami.ami.id
-  instance_type            = "t3.small"
+  instance_type            = var.instance_type
   vpc_security_group_ids   = [ "sg-0c786c9e56b3a46f3" ]
 
 }
 
-# variable "instance_type" {}
+ variable "instance_type" {}

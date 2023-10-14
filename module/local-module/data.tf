8c2594099f004e4c430fa983a4362cb63f249b11
chinnapareddy <chinna.aduri@gmail.com>
2023-10-14 03:37:05 -0700

    Module

--- module/local-module/data.tf
+++ module/local-module/data.tf
@@ -1,4 +1,4 @@
-data "aws_ami" "ami" {
+data "aws_ami_ids" "ami" {
   most_recent = true
   name_regex  = "Centos-8-DevOps-Practice"
   owners      = ["683468297985"]
@@ -6,11 +6,11 @@ data "aws_ami" "ami" {
 
 
 output "ami" {
-  value = data.aws_ami.ami
+  value = data.aws_ami_ids.ami
 }
 
 resource "aws_instance" "instance" {
-   ami                     = data.aws_ami.ami.id
+   ami                     = data.aws_ami_ids.ami.id
   instance_type            = var.instance_type
   vpc_security_group_ids   = [ "sg-0c786c9e56b3a46f3" ]
 

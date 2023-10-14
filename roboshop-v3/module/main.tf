f5e0d92e2da9f549215a7aa68c09ced31d647a43
chinnapareddy <chinna.aduri@gmail.com>
2023-10-14 07:34:39 -0700

    Module roboshop

--- roboshop-v3/module/main.tf
+++ roboshop-v3/module/main.tf
@@ -26,7 +26,7 @@ resource "null_resource" "ansible" {
 
   provisioner "local-exec" {
     command = <<EOF
-cd /home/centos/roboshop-ansible
+cd /home/centos/p1-roboshop-ansible1
 git pull
 sleep 30
 ansible-playbook -i ${var.name}-dev.devops-tools.online, main.yml -e ansible_user=centos -e ansible_password=DevOps321 -e component=${var.name}

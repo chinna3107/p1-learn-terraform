0c1e6093e741c867a12b9df69595fc66f91085bf
chinnapareddy <chinna.aduri@gmail.com>
2023-10-14 07:06:43 -0700

    Module roboshop

--- roboshop-v3/module/main.tf
+++ roboshop-v3/module/main.tf
@@ -7,10 +7,6 @@ resource "aws_instance" "instance" {
     Name = var.name
   }
 
-  provisioner  "local-exec" {
-    command = "sleep 60"
-  }
-
 }
 
 
@@ -20,4 +16,20 @@ resource "aws_route53_record" "record" {
   type    = "A"
   ttl     = 30
   records = [aws_instance.instance.private_ip]
+}
+
+resource "null_resource" "ansible" {
+
+  depends_on = [
+    aws_route53_record.record
+  ]
+
+  provisioner "local-exec" {
+    command = <<EOF
+cd /home/centos/roboshop-ansible
+git pull
+sleep 30
+ansible-playbook -i ${var.name}-dev.devops-tools.online, main.yml -e ansible_user=centos -e ansible_password=DevOps321 -e component=${var.name}
+EOF
+  }
 }

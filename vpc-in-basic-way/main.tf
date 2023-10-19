c7edf400384c65ff56a2163d3cf88786aca737a8
chinnapareddy <chinna.aduri@gmail.com>
2023-10-18 22:51:34 -0700

    terraform structure

--- vpc-in-basic-way/main.tf
+++ vpc-in-basic-way/main.tf
@@ -7,11 +7,11 @@ variable "public_subnets" {
 }
 
 variable "app_subnets" {
-  default = [ "10.0.0.0/24", "10.0.1.0/24"]
+  default = [ "10.0.2.0/24", "10.0.3.0/24"]
 }
 
 variable "db_subnets" {
-  default = [ "10.0.0.0/24", "10.0.1.0/24"]
+  default = [ "10.0.4.0/24", "10.0.5.0/24"]
 }
 
 resource "aws_vpc" "main" {
